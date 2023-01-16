a698bee7ed3ad791742e5cc923b4ef692e328f61
alexander <castro.t.alex@gmail.com>
2023-01-16 16:55:34 -0500

    Adding a verification empty inputs

--- components/Transaction/index.jsx
+++ components/Transaction/index.jsx
@@ -10,13 +10,18 @@ const index = () => {
 
 
   function handleSubmit(e){
-    e.preventDefault()
+   
+    e.preventDefault() 
+    if(amount === 0) return
     const newTransaction = {
       id: nanoid(),
       text,
       amount: +amount
     }
     addTransaction(newTransaction);
+    setAmount(0)
+    setText('')
+
   }
 
 
@@ -25,9 +30,9 @@ const index = () => {
       <h3 className={styles.title}>Add new transaction</h3>
       <form className={styles.formContainer} onSubmit={handleSubmit}>
         <label htmlFor="text">Text</label>
-        <input type="text" id='text' placeholder='Enter product...' value={text} onChange={(e)=>setText(e.target.value)}/>
+        <input required type="text" id='text' placeholder='Enter product...' value={text} onChange={(e)=>setText(e.target.value)}/>
         <label htmlFor="amount">Amount</label>
-        <input type="number" id='amount' placeholder='Enter amount...' value={amount} onChange={(e)=>setAmount(e.target.value)} />
+        <input required type="number" id='amount' placeholder='Enter amount...' value={amount} onChange={(e)=>setAmount(e.target.value)} />
         <button>Add transaction</button>
       </form>
     </section>
